0a7069961cc9b9b6a4fecd404768974713645c9f
samirandeb <samirandeb79@gmail.com>
2020-10-03 19:09:33 +0530

    updated file

--- sam-function-demo/src/main/java/com/sam/function/SamFunctionDemoApplication.java
+++ sam-function-demo/src/main/java/com/sam/function/SamFunctionDemoApplication.java
@@ -4,14 +4,10 @@ import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.context.FunctionRegistration;
-import org.springframework.cloud.function.context.FunctionType;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
-public class SamFunctionDemoApplication implements ApplicationContextInitializer<GenericApplicationContext>{
+public class SamFunctionDemoApplication /* implements ApplicationContextInitializer<GenericApplicationContext> */{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamFunctionDemoApplication.class, args);
@@ -27,11 +23,11 @@ public class SamFunctionDemoApplication implements ApplicationContextInitializer
 		return value -> new StringBuilder(value).reverse().toString();
 	}
 
-	@Override
+	/* @Override
 	public void initialize(GenericApplicationContext ctx) {
 		ctx.registerBean("demo", FunctionRegistration.class,
         () -> new FunctionRegistration<>(uppercase())
             .type(FunctionType.from(String.class).to(String.class)));
-	}
+	} */
 
 }
